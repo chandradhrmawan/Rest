60b56a76bcdb1f937f291b9a8dc229031526b5c0
dessurya <fourline66@gmail.com>
2019-11-08 14:27:05 +0700

    Update ConnectedExternalApps.php

--- app/Helper/ConnectedExternalApps.php
+++ app/Helper/ConnectedExternalApps.php
@@ -292,7 +292,7 @@ class ConnectedExternalApps{
                   "timestamp": "YYYYMMDD HH:Mi:SS"
               },
               "esbBody": {
-                  "vTruckId": "'.$input['truck_plat_no'].'",
+                  "vTruckId": "'. str_replace(' ','',$input['truck_plat_no']).'",
                   "vTruckNumber": "'.$input['truck_plat_no'].'",
                   "vRfidCode": "'.$input['truck_rfid_code'].'",
                   "vCustomerName": "'.$input['customer_name'].'",
