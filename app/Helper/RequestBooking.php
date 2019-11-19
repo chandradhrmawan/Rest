88d1cac8592d035e533f584e06a5febd98a14391
dessurya <fourline66@gmail.com>
2019-11-19 13:52:32 +0700

    Update RequestBooking.php

--- app/Helper/RequestBooking.php
+++ app/Helper/RequestBooking.php
@@ -300,7 +300,7 @@ class RequestBooking{
 		if ($migrateTariff == true) {
 			$pesan = "Created Uper No : ".$headU->uper_no;
 		}else if($migrateTariff == false) {
-			ConnectedExternalApps::sendRequestBooking($find[$config['head_no']]);
+			ConnectedExternalApps::sendRequestBooking(['req_no' => $find[$config['head_no']], 'paid_date' => null ]);
 			$pesan = "Uper Not created, uper percent for this request is 0%";
 		}
 
