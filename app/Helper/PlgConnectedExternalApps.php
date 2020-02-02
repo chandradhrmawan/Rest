c03b5093b767dd886b4aa605ce9807e861a761ee
dessurya <fourline66@gmail.com>
2020-02-02 16:51:31 +0700

    Update PlgConnectedExternalApps.php

--- app/Helper/PlgConnectedExternalApps.php
+++ app/Helper/PlgConnectedExternalApps.php
@@ -160,7 +160,7 @@ class PlgConnectedExternalApps{
 			$config = json_decode($config->api_set, true);
 			$getReq = DB::connection('omuster')->table($config['head_table'])->where($config['head_no'],$getNota->nota_req_no)->first();
 			$getReq = (array)$getReq;
-			if ($getReq[$config['head_paymethod']] == 2) {
+			if ($getReq[$config['head_paymethod']] == 1) {
 				$sendRequestBooking = static::sendRequestBookingPLG(['id' => $getReq[$config['head_primery']] ,'config' => $config]);
 			}
 	    	// sementara di by pass
