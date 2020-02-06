644059f288765d0a3fd35482f329d4f1f26fe0ee
dessurya <fourline66@gmail.com>
2020-02-06 11:24:10 +0700

    Update PlgRequestBooking.php

--- app/Helper/PlgRequestBooking.php
+++ app/Helper/PlgRequestBooking.php
@@ -95,7 +95,11 @@ class PlgRequestBooking{
 			}else{
 				$newD['DTL_BL'] = empty($list[$config['DTL_BL']]) ? 'NULL' : strtoupper($list[$config['DTL_BL']]);
 			}
-			$newD['DTL_PKG_ID'] = 8;
+			if (empty($config['DTL_PKG_ID'])) {
+				$newD['DTL_BL'] = 8;
+			}else{
+				$newD['DTL_PKG_ID'] = empty($list[$config['DTL_PKG_ID']]) ? 'NULL' : $list[$config['DTL_PKG_ID']];
+			}
 			if (empty($config['DTL_CMDTY_ID'])) {
 				$newD['DTL_CMDTY_ID'] = 'NULL';
 			}else{
