78d5802fe8c3745827af47399709ffbeee998586
Chalid Ade Rahman <chalidade@gmail.com>
2020-02-10 15:43:17 +0700

    getplacement scheduller

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -43,7 +43,7 @@ class Kernel extends ConsoleKernel
 
       $schedule->call('App\Helper\PlgConnectedExternalApps@getRealGati');
 
-      // $schedule->call('App\Helper\PlgConnectedExternalApps@getRealPlacement');
+      $schedule->call('App\Helper\PlgConnectedExternalApps@getUpdatePlacement');
 
     }
 }
