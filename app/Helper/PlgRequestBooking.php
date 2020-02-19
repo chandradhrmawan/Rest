ba8874c9f0018c7e31a37d0605f2836ec19f79a0
dessurya <fourline66@gmail.com>
2020-02-19 11:15:50 +0700

    Update PlgRequestBooking.php

--- app/Helper/PlgRequestBooking.php
+++ app/Helper/PlgRequestBooking.php
@@ -666,7 +666,7 @@ class PlgRequestBooking{
 	    }
 
 	    public static function approvalProformaPLG($input){
-            $getNota = TxHdrNota::find($input['nota_id']);
+            $getNota = DB::connection('omuster')->table('TX_HDR_NOTA')->where('nota_id',$input['nota_id'])->first();
             if (empty($getNota)) {
             	return ['result' => "Fail, proforma not found!", "Success" => false];
             }
