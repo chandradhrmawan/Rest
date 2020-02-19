86aa705b808972f1334993af5e53dd63500378ab
dessurya <fourline66@gmail.com>
2020-02-19 15:14:42 +0700

    Update PlgRequestBooking.php

--- app/Helper/PlgRequestBooking.php
+++ app/Helper/PlgRequestBooking.php
@@ -682,7 +682,7 @@ class PlgRequestBooking{
             	$arr = [
             		'nota' => (array)$getNota,
             	];
-            	return $sendInvProforma = PlgEInvo::sendInvProforma($arr);
+            	$sendInvProforma = PlgEInvo::sendInvProforma($arr);
             	if ($sendInvProforma['Success'] == true) {
 	            	$getNota->nota_status = 2;
 	            	$getNota->save();
