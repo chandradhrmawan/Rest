0008c57629a180158f97555141d1e6968c5a25ee
dessurya <fourline66@gmail.com>
2020-02-24 10:31:29 +0700

    Update PlgRequestBooking.php

--- app/Helper/PlgRequestBooking.php
+++ app/Helper/PlgRequestBooking.php
@@ -617,6 +617,7 @@ class PlgRequestBooking{
         	$sendInvPay = PlgEInvo::sendInvPay($arr);
         	if (empty($sendInvPay['Success']) or $sendInvPay['Success'] == false) {
         		return [
+        			'Success' => false,
         			'response' => 'Fail, cant send payment invoice',
         			'no_pay' => $pay->pay_no,
         			'nota_no' => $getNota->nota_no,
