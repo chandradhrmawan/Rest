0a2bc642097d67855530009daf069720f6efb3c2
dessurya <fourline66@gmail.com>
2020-02-27 10:38:26 +0700

    Update PlgRequestBooking.php

--- app/Helper/PlgRequestBooking.php
+++ app/Helper/PlgRequestBooking.php
@@ -550,7 +550,7 @@ class PlgRequestBooking{
         		$sendRequestBooking = PlgFunctTOS::sendRequestBookingPLG(['id' => $id, 'table' => $table, 'config' => $config]);
         		if (!empty($cekIsCanc)) {
         			DB::connection('omuster')->table('TX_HDR_CANCELLED')->where('cancelled_id', $cekIsCanc['cancelled_id'])->update([
-        				'cancelled_status' => 4
+        				'cancelled_status' => 6
         			]);
         		}
         	}
